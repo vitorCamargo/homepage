5d966fc463719d1536ba0a77274736e35ec4e60a
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-10-11 09:46:08 -0300

    Initial Structure

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id = "app">
+    <span style = "font-weight: 600;
+font-size: 64px;
+line-height: 96px;">Hi. I'm Vitor</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
