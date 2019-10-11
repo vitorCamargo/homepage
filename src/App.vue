2963e3183bfa9380f40c0db1cc524fc65809f726
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-10-11 19:55:46 -0300

    Info Banner Finished

--- src/App.vue
+++ src/App.vue
@@ -6,40 +6,54 @@
           <img src = "./assets/logo.png" class = "menu-logo" />
         </a>
 
-        <div class = "navbar mobile-menu">
-          <div v-bind:class = "scrollPage === 'intro' ? 'item-selected' : 'item'"> <a href = "#intro"> Intro </a> </div>
-          <div v-bind:class = "scrollPage === 'aboutMe' ? 'item-selected' : 'item'"> <a href = "#aboutMe"> {{ lang === 'en' ? 'About me' : 'Sobre' }} </a> </div>
-          <div v-bind:class = "scrollPage === 'skills' ? 'item-selected' : 'item'"> <a href = "#skills"> {{ lang === 'en' ? 'Skills' : 'Habilidades' }} </a> </div>
-          <div v-bind:class = "scrollPage === 'portfolio' ? 'item-selected' : 'item'"> <a href = "#portfolio"> {{ lang === 'en' ? 'Portfolio' : 'Portifólio' }} </a> </div>
-          <div v-bind:class = "scrollPage === 'contact' ? 'item-selected' : 'item'"> <a> {{ lang === 'en' ? 'Contact' : 'Contato' }} </a> </div>
-        </div>
+        <div style = "display: flex;">
+          <div class = "navbar mobile-menu" style = "margin-right: 50px;">
+            <div v-bind:class = "scrollPage === 'intro' ? 'item-selected' : 'item'"> <a href = "#intro"> Intro </a> </div>
+            <div v-bind:class = "scrollPage === 'aboutMe' ? 'item-selected' : 'item'"> <a href = "#aboutMe"> {{ lang === 'en' ? 'About me' : 'Sobre' }} </a> </div>
+            <div v-bind:class = "scrollPage === 'skills' ? 'item-selected' : 'item'"> <a href = "#skills"> {{ lang === 'en' ? 'Skills' : 'Habilidades' }} </a> </div>
+            <div v-bind:class = "scrollPage === 'portfolio' ? 'item-selected' : 'item'"> <a href = "#portfolio"> {{ lang === 'en' ? 'Portfolio' : 'Portifólio' }} </a> </div>
+            <div v-bind:class = "scrollPage === 'contact' ? 'item-selected' : 'item'"> <a> {{ lang === 'en' ? 'Contact' : 'Contato' }} </a> </div>
+          </div>
 
-        <div class = "language mobile-menu" @mouseover = "languageDropdownVisible = true" @mouseleave = "languageDropdownVisible = false">
-          <img v-if = "lang === 'en'" src = "./assets/usa.svg" class = "language-flag">
-          <img v-else src = "./assets/brazil.svg" class = "language-flag">
+          <div class = "language mobile-menu" @mouseover = "languageDropdownVisible = true" @mouseleave = "languageDropdownVisible = false">
+            <img v-if = "lang === 'en'" src = "./assets/usa.svg" class = "language-flag">
+            <img v-else src = "./assets/brazil.svg" class = "language-flag">
 
-          <span class = "language-text"> {{ lang === 'en' ? 'EN' : 'PT' }} </span>
+            <span class = "language-text"> {{ lang === 'en' ? 'EN' : 'PT' }} </span>
 
-          <img src = "./assets/dropdown.svg" style = "margin-top: 7px;">
+            <img src = "./assets/dropdown.svg" style = "margin-top: 7px;">
 
-          <div class = "language-dropdown" v-show = "languageDropdownVisible">
-            <span class = "language-dropdown-item language-dropdown-item-first" @click = "lang = 'en'; languageDropdownVisible = false;">
-              <img src = "./assets/usa.svg" style = "margin-right: 10px;" class = "language-flag">
+            <div class = "language-dropdown" v-show = "languageDropdownVisible">
+              <span class = "language-dropdown-item language-dropdown-item-first" @click = "lang = 'en'; languageDropdownVisible = false;">
+                <img src = "./assets/usa.svg" style = "margin-right: 10px;" class = "language-flag">
 
-              <span class = "language-text"> English </span>
-            </span>
+                <span class = "language-text"> English </span>
+              </span>
 
-            <span class = "language-dropdown-item language-dropdown-item-last" @click = "lang = 'pt'; languageDropdownVisible = false;">
-              <img src = "./assets/brazil.svg" style = "margin-right: 10px;" class = "language-flag">
+              <span class = "language-dropdown-item language-dropdown-item-last" @click = "lang = 'pt'; languageDropdownVisible = false;">
+                <img src = "./assets/brazil.svg" style = "margin-right: 10px;" class = "language-flag">
 
-              <span class = "language-text"> Português </span>
-            </span>
+                <span class = "language-text"> Português </span>
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </a-affix>
 
-    <div ref = "intro" id = "intro" style = "padding-top: 700px; height: 100px;">kkk</div>
+    <div ref = "intro" id = "intro">
+      <img src = "./assets/me.svg" style = "max-width: 600px; width: 100%;" />
+
+      <div class = "intro-box">
+        <div style = " text-align: left; margin-bottom: 30px;">
+          <span class = "info-title"> {{ lang === 'en' ? 'Hi. I\'m Vitor' : 'Oi. Eu sou Vitor' }} </span><br />
+          <span class = "info-subtitle"> {{ lang === 'en' ? 'A Computer Science Padawan' : 'Apenas um estudante de Ciência da Computação' }} </span><br />
+        </div>
+
+        <a href = "#portfolio" class = "button-primary"> {{ lang === 'en' ? 'View Portfolio' : 'Ver Projetos' }} </a>
+      </div>
+    </div>
+
     <div ref = "aboutMe" id = "aboutMe" style = "padding-top: 700px; height: 100px;">kkk</div>
     <div ref = "skills" id = "skills" style = "padding-top: 700px; height: 100px;">kkk</div>
     <div ref = "portfolio" id = "portfolio" style = "padding-top: 700px; height: 100px;">kkk</div>
@@ -153,6 +167,7 @@
   }
 
   .navbar .item {
+    font-weight: 300;
     margin-left: 20px;
     margin-right: 20px;
     cursor: pointer;
@@ -227,8 +242,7 @@
   }
 
   .language-dropdown-item:hover .language-text {
-    font-weight: bolder;
-    font-style: italic;
+    font-weight: 600;
   }
 
   .language-dropdown {
@@ -256,6 +270,67 @@
     transition-timing-function: ease-out;
   }
 
+  #intro {
+    background: #FFF url(./assets/background-info.svg);
+    background-position: bottom;
+    background-size: cover;
+    padding: 20px;
+    padding-bottom: 150px;
+    margin-top: 80px;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .intro-box {
+    max-width: 350px;
+    width: 100%;
+    margin-top: auto;
+    margin-bottom: auto;
+    text-align: center;
+  }
+
+  .info-title {
+    font-weight: 800;
+    font-size: 42px;
+  }
+
+  .info-subtitle {
+    font-weight: 100;
+    font-size: 21px;
+    color: #666;
+  }
+
+  .button-primary {
+    background: linear-gradient(287.37deg, #404DE6 -20.96%, #72E9F3 125.08%);
+    box-shadow: 0px 0px 27px rgba(64, 77, 230, 0.3);
+    border-radius: 105px;
+    border: none;
+    padding: 10px 30px;
+    margin-top: 22px;
+    color: #FFF;
+    cursor: pointer;
+    outline: none;
+  }
+
+  .button-primary:active, .button-primary:hover {
+    color: #FFF;
+  }
+  @media only screen and (max-width: 390px) {
+    .intro-box {
+      margin-top: -21px !important;
+    }
+  }
+
+  @media only screen and (max-width: 460px) {
+    .info-title {
+      font-size: 31px;
+    }
+
+    .info-subtitle {
+      font-size: 16px;
+    }
+  }
+
   @media only screen and (max-width: 820px) {
     .mobile-menu {
       display: none !important;
@@ -263,6 +338,20 @@
 
     .mobile-menu-on {
       display: block;
+    }
+  }
+
+  @media only screen and (max-width: 1040px) {
+    #intro {
+      display: block;
+      text-align: center;
+    }
+
+    .intro-box {
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: -28px;
+      margin-bottom: -30px;
     }
   }
 </style>
